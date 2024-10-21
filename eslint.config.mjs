da4755930f1a0bdf35ce4d903d8827dc10e21168
Magic Len <len@magiclen.org>
2024-10-21 16:05:00 +0800

    adjust eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -293,6 +293,7 @@ export default tseslint.config(
                     "tests/**/*",
                     "**/*.test.ts",
                     "**/*.spec.ts",
+                    "webpack.config.ts",
                 ],
             }],
  
